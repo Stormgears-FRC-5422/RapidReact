93d190c88f406ef06cc87b92818712f8894b123f
neonlit008 <63197592+neonlit008@users.noreply.github.com>
2022-01-24 20:42:54 -0500

    New subsystem for NavX, hopefully it works may have to add a calibration method
    had to add navx file to git

---
+++ src/main/java/frc/robot/subsystems/NavX.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import utils.StormProp;
+
+public class NavX extends SubsystemBase {
+    //AHRS is the thingy for connectivity and to access state information
+    private AHRS ahrs = null;
+
+    public NavX() {
+        if(StormProp.getBoolean("hasNavX", true)) {
+            ahrs = new AHRS();
+            ahrs.enableLogging(true);
+        } else {
+            System.out.println("NO NAVX IN USEEEEEEEEEEEE");
+        }
+    }
+
+    public double getAngle() {
+        return ahrs.getAngle();
+    }
+}
