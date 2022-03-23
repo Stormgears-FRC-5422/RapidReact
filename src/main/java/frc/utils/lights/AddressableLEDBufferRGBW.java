de4f247ccc83ac9b17d805c4a966e6d913d95b20
Darren Miller <darrenleomiller@gmail.com>
2022-03-23 13:06:34 -0400

    Refactored Addressable LED RGBW handler - now a subclass of AddressableLEDBuffer.

--- src/main/java/frc/utils/lights/AddressableLEDBufferRGBW.java
+++ src/main/java/frc/utils/lights/AddressableLEDBufferRGBW.java
@@ -1,38 +1,36 @@
 package frc.utils.lights;
 
-// Based on the AddressableLEDBuffer code from wpilib. Unfortunately I couldn't just subclass it, and they didn't make an interface...
+// The AddressableLED approach that WPI provides is intended for RGB lights. Stormgears has a lot of RGBW lights
+// (lots of variations, GRBW might actually be more appropriate for this code...)
 // The good news is that the protocol is really easy - the main part is just sending an array of bytes, so we can fool the
-// RGB variant by sending the right bytes along.
+// RGB variant by sending the right bytes down the wire.
 // To use, the example code does something like this in robotInit();
 //    Specify PWM port - Must be a PWM header, not MXP or DIO
 //    m_led = new AddressableLED(0);
 //
 //    // Reuse buffer
 //    // Length is expensive to set, so only set it once, then just update data
-//    m_ledBuffer = new RGBWBufferWrapper(16);
+//    m_ledBuffer = new AddressableLEDBufferRGBW(16);
 //
 //    // Call the effectiveLength to get the correct number of bytes set up. As opposed to length() which is the number of RGBW devices
 //    m_led.setLength(m_ledBuffer.getEffectiveLength());
 //
 //    // Set the data - you can do this in periodic, too.
-//    m_led.setData(m_ledBuffer.getLEDBuffer());
-//    // Only need to start onces
+//    m_led.setData(m_ledBuffer);
+//    // Only need to start once
 //    m_led.start();
 
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 
-public class RGBWBufferWrapper {
-    AddressableLEDBuffer ledBuffer;
+public class AddressableLEDBufferRGBW extends AddressableLEDBuffer {
     int ledLength;
 
-
-    public RGBWBufferWrapper(int length) {
+    public AddressableLEDBufferRGBW(int length) {
+        super( (length * 4 + 2) / 3 );
         ledLength = length;
-        ledBuffer = new AddressableLEDBuffer( (length * 4 + 2) / 3 );
     }
-
 
     public void setRGBW(int index, int r, int g, int b, int w) {
         // actual array order for the NEOPixels we have is GRBW
@@ -57,99 +55,39 @@ public class RGBWBufferWrapper {
         int j = aG / 3;
 
         // Stash the current color, if any. We need to merge our new colors into these slots
-        c0 = ledBuffer.getLED8Bit(j);
-        c1 = ledBuffer.getLED8Bit(j+1);
+        c0 = super.getLED8Bit(j);
+        c1 = super.getLED8Bit(j+1);
 
         // which byte are we aligned with in the RGB world? Set the new colors, but keep the other bytes as-is
         switch (aG % 3) {
             case 0:
-                ledBuffer.setRGB( j, r, g, b);
-                ledBuffer.setRGB(j+1, c1.red, w, c1.blue);
+                super.setRGB( j, r, g, b);
+                super.setRGB(j+1, c1.red, w, c1.blue);
                 break;
             case 1:
-                ledBuffer.setRGB( j, g, c0.green, r);
-                ledBuffer.setRGB(j+1, w, b, c1.blue);
+                super.setRGB( j, g, c0.green, r);
+                super.setRGB(j+1, w, b, c1.blue);
                 break;
             case 2:
-                ledBuffer.setRGB( j, c0.red, c0.green, g);
-                ledBuffer.setRGB(j+1, b, r, w);
+                super.setRGB( j, c0.red, c0.green, g);
+                super.setRGB(j+1, b, r, w);
                 break;
         }
 
     }
 
-
+    @Override
     public void setRGB(int index, int r, int g, int b) {
         setRGBW(index, r,g,b,0);
     }
 
-
-    public void setLED(int index, Color8Bit color) {
-        setRGB(index, color.red, color.green, color.blue);
-    }
-
-
-    public void setLED(int index, Color color) {
-        setRGB(index, (int) (color.red * 127), (int) (color.green * 127), (int) (color.blue * 127));
-    }
-
-    /**
-     * Sets a specific led in the buffer.
-     *
-     * @param index the index to write
-     * @param h the h value [0-180]
-     * @param s the s value [0-255]
-     * @param v the v value [0-255]
-     */
-    @SuppressWarnings("ParameterName")
-    public void setHSV(final int index, final int h, final int s, final int v) {
-        if (s == 0) {
-            setRGB(index, v, v, v);
-            return;
-        }
-
-        final int region = h / 30;
-        final int remainder = (h - (region * 30)) * 6;
-
-        final int p = (v * (255 - s)) >> 8;
-        final int q = (v * (255 - ((s * remainder) >> 8))) >> 8;
-        final int t = (v * (255 - ((s * (255 - remainder)) >> 8))) >> 8;
-
-        switch (region) {
-            case 0:
-                setRGB(index, v, t, p);
-                break;
-            case 1:
-                setRGB(index, q, v, p);
-                break;
-            case 2:
-                setRGB(index, p, v, t);
-                break;
-            case 3:
-                setRGB(index, p, q, v);
-                break;
-            case 4:
-                setRGB(index, t, p, v);
-                break;
-            default:
-                setRGB(index, v, p, q);
-                break;
-        }
-    }
-
-
+    @Override
     public int getLength() {
         return ledLength;
     }
 
-
     public int getEffectiveLength() {
-        return ledBuffer.getLength();
-    }
-
-
-    public AddressableLEDBuffer getLEDBuffer() {
-        return ledBuffer;
+        return super.getLength();
     }
 
 }
