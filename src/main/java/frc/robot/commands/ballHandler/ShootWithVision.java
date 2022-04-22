3a2c27e75274c564536d1e0732a44a118751232d
Veer Shah <veer.shah1016@gmail.com>
2022-04-21 23:25:31 -0500

    added moving average to the log?? i dont know if the moving average works though looks like it would just keep increasing?
    Signed-off-by: Veer Shah <veer.shah1016@gmail.com>

--- src/main/java/frc/robot/commands/ballHandler/ShootWithVision.java
+++ src/main/java/frc/robot/commands/ballHandler/ShootWithVision.java
@@ -5,6 +5,7 @@ import edu.wpi.first.util.datalog.DoubleArrayLogEntry;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.ballHandler.Shooter;
+import frc.utils.filters.MovingAverage;
 import io.github.oblarg.oblog.Loggable;
 import io.github.oblarg.oblog.annotations.Config;
 import io.github.oblarg.oblog.annotations.Log;
@@ -20,9 +21,10 @@ public class ShootWithVision extends CommandBase implements Loggable {
   private final DoubleSupplier distance;
   @Log.Exclude private final Shooter shooter;
   @Log.Exclude private final Shoot shoot;
-  double magicConstant;
-  double shooterSetpointChangingThreshold;
+  private final MovingAverage setpointMovingAverage;
+  private double magicConstant;
   private DoubleArrayLogEntry shooterDistanceRPSLog;
+  private double shooterSetpointChangingThreshold;
 
   public ShootWithVision(
       Shooter shooter,
@@ -36,6 +38,7 @@ public class ShootWithVision extends CommandBase implements Loggable {
     this.distance = distance;
     magicConstant = kMagicVisionConstant;
     shooterSetpointChangingThreshold = kShooterSetpointChangingThreshold;
+    setpointMovingAverage = new MovingAverage(25);
     if (shooterDistanceRPSLog != null) this.shooterDistanceRPSLog = shooterDistanceRPSLog;
     for (Subsystem requirement : shoot.getRequirements()) {
       addRequirements(requirement);
@@ -65,30 +68,34 @@ public class ShootWithVision extends CommandBase implements Loggable {
     return 41.1 * Math.pow(feet, 0.247);
   }
 
-  // TODO if setpoint still oscillating, make moving average with 20 samples, and update average
-  // with samples... only update setpoint if the average is + or - 0.5 rps
   private void updateSetpoint(boolean change) {
     if (hasTarget.getAsBoolean()) {
       double distanceMeters = distance.getAsDouble() / magicConstant;
       double rps = metersToRPS(distanceMeters);
+      if (change) setpointMovingAverage.force(rps);
+      else setpointMovingAverage.update(rps);
+      // TODO for moving average change rps on next two lines to movingAverage.getValue() & change
+      // threshold to (0.5)?
+      // For now, just logging the moving average can change for matches 6-10
       if (change || Math.abs(shooter.setpoint() - rps) > shooterSetpointChangingThreshold) {
         shooter.setSetpoint(rps);
         try {
           System.out.println("Distance: " + distanceMeters + " and shooting @ " + rps + " rps");
-          double[] logEntry = new double[] {distanceMeters, rps};
+          double[] logEntry = new double[] {distanceMeters, rps, setpointMovingAverage.getValue()};
           shooterDistanceRPSLog.append(logEntry);
         } catch (Exception e) {
           e.printStackTrace();
         }
       }
-    } else
+    } else {
       try {
         System.out.println("No recorded distance and shooting @ " + shooter.getSpeed() + " rps");
-        double[] logEntry = new double[] {0, shooter.setpoint()};
+        double[] logEntry = new double[] {0, shooter.setpoint(), setpointMovingAverage.getValue()};
         shooterDistanceRPSLog.append(logEntry);
       } catch (Exception e) {
         e.printStackTrace();
       }
+    }
   }
 
   @Config(defaultValueNumeric = 1.8)
